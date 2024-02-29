1ce482fcdcd705b9c16b8707e6c6b124d503d099
Ilya <rabota003@yandex.ru>
2024-02-29 21:06:07 +0300

    one commit

---
+++ src/main/java/me/ishutin/dadata/DadataApplication.java
@@ -0,0 +1,13 @@
+package me.ishutin.dadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/ishutin/dadata/DadataApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ishutin.dadata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
